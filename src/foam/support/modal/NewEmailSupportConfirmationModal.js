1a977180e6e94b4b16fafe7e8140cf05fc976879
Patrick <zahnno@gmail.com>
2018-06-12 16:33:44 -0400

    refactored newEmailSupportConfirmationModal

--- src/foam/support/modal/NewEmailSupportConfirmationModal.js
+++ src/foam/support/modal/NewEmailSupportConfirmationModal.js
@@ -11,85 +11,32 @@ foam.CLASS({
   ],
   
   imports: [
-    'ctrl',
-    'closeDialog'
+    'closeDialog',
+    'ctrl'
   ],
 
   css:`
-    ^ {
-      height: 260px;
-    }
-    ^ .title {
-      margin-left: 20px;
-      width: 198px;
-      height: 40px;
-      font-family: Roboto;
-      font-size: 14px;
-      font-weight: normal;
-      font-style: normal;
-      font-stretch: normal;
-      line-height: 2.86;
-      letter-spacing: 0.2px;
-      text-align: left;
-      color: #ffffff;
-    }
-    ^ .label1 {
+    ^ .label {
       width: 395px;
       height: 16px;
-      font-family: Roboto;
       font-size: 12px;
-      font-weight: normal;
-      font-style: normal;
-      font-stretch: normal;
-      line-height: 1.33;
-      letter-spacing: 0.2px;
-      text-align: left;
       color: #093649;
-      margin-top:20px;
       margin-left:20px;
       margin-bottom: 79px;
     }
-    ^ .Mask {
-      width: 600px;
-      height: 180px;
-      border-radius: 2px;
-      background-color: #ffffff;
-    }
-    ^ .Rectangle-13 {
-      width: 448px;
-      height: 40px;
-      background-color: #093649;
-    }
-    ^ .input {
-      margin-top: 15px;
-      margin-bottom: 79px;
-      width: 408px;
-      height: 40px;
-      background-color: #ffffff;
-      border: solid 1px rgba(164, 179, 184, 0.5);
-    }
-    ^ .Rectangle-8 {
-      width: 135px;
-      height: 40px;
-      border-radius: 2px;
+    ^ .blue-button{
+      border: none;
       background-color: #59a5d5;
-      font-family: Roboto;
-      font-size: 14px;
-      font-weight: normal;
-      font-style: normal;
-      font-stretch: normal;
-      line-height: 2.86;
-      letter-spacing: 0.2px;
-      text-align: center;
       color: #ffffff;
-      margin-left: 157px;
-      margin-top:50px;
+      margin: 20px 150px;
+      float: right;
+      font-size: 13px;    
     }
     `,
 
     messages:[
-      {name:'title', message:'New Email'},
-      {name:'titlelabel', message:'Please go to the email box to validate the email address before you can connect to the help desk.'},  
+      {name:'Title', message:'New Email'},
+      {name:'Titlelabel', message:'Please go to the email box to validate the email address before you can connect to the help desk.'},  
     ],
         
     methods:[
@@ -97,11 +44,11 @@ foam.CLASS({
         this.addClass(this.myClass())
         this
         .tag(this.ModalHeader.create({
-          title: 'New Email'
+          title: this.Title
         }))
-        .start().add(this.titlelabel).addClass('label1')
+        .start().add(this.Titlelabel).addClass('label')
           .end()
-          .start(this.CLOSE_MODAL).addClass('Rectangle-8')
+          .start(this.CLOSE_MODAL).addClass('btn').addClass('blue-button')
           .end()
         .end();
       }
